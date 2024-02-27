ce1e89381bee85e13c763ae332b6ffacefe28426
ubinix-warun <warun@ubinix.com>
2024-02-27 23:35:11 +0700

    Add 'interact.ts' for deploy OracleContract

--- src/interact.ts
+++ src/interact.ts
@@ -1,108 +1,84 @@
-/**
- * This script can be used to interact with the Add contract, after deploying it.
- *
- * We call the update() method on the contract, create a proof and send it to the chain.
- * The endpoint that we interact with is read from your config.json.
- *
- * This simulates a user interacting with the zkApp from a browser, except that here, sending the transaction happens
- * from the script and we're using your pre-funded zkApp account to pay the transaction fee. In a real web app, the user's wallet
- * would send the transaction and pay the fee.
- *
- * To run locally:
- * Build the project: `$ npm run build`
- * Run with node:     `$ node build/src/interact.js <deployAlias>`.
- */
 import fs from 'fs/promises';
-import { Mina, NetworkId, PrivateKey } from 'o1js';
-import { Add } from './Add.js';
+import { Toolkit, OracleContract } from "@zkoracle/opennautilus-contracts"
 
-// check command line arg
-let deployAlias = process.argv[2];
-if (!deployAlias)
-  throw Error(`Missing <deployAlias> argument.
+import { Mina, PrivateKey, Signature } from 'o1js';
+// import { OracleContract } from '.js';
 
-Usage:
-node build/src/interact.js <deployAlias>
-`);
-Error.stackTraceLimit = 1000;
-const DEFAULT_NETWORK_ID = 'testnet';
-
-// parse config and private key from file
-type Config = {
-  deployAliases: Record<
-    string,
-    {
-      networkId?: string;
-      url: string;
-      keyPath: string;
-      fee: string;
-      feepayerKeyPath: string;
-      feepayerAlias: string;
+// Network configuration
+let config = new Map([
+  [
+    "lightnet", {
+      network: {
+        mina: 'http://localhost:8080/graphql',
+        archive: 'http://localhost:8282',
+        lightnetAccountManager: 'http://localhost:8181'
+      },
+      fee: Number("0.1") * 1e9 // in nanomina (1 billion = 1.0 mina)
     }
-  >;
-};
-let configJson: Config = JSON.parse(await fs.readFile('config.json', 'utf8'));
-let config = configJson.deployAliases[deployAlias];
-let feepayerKeysBase58: { privateKey: string; publicKey: string } = JSON.parse(
-  await fs.readFile(config.feepayerKeyPath, 'utf8')
-);
+  ],
+  [
+    "berkeley", {
+      network: {
+        mina: 'https://proxy.berkeley.minaexplorer.com/graphql',
+        archive: 'https://api.minascan.io/archive/berkeley/v1/graphql/',
+        // lightnetAccountManager: 'http://localhost:8181'
+      },
+      fee: Number("0.1") * 1e9 // in nanomina (1 billion = 1.0 mina)
+    }
+  ],
+  [
+    "testworld", {
+      network: {
+        mina: 'https://proxy.testworld.minaexplorer.com/graphql',
+        archive: 'https://api.minascan.io/archive/testworld/v1/graphql/',
+        // lightnetAccountManager: 'http://localhost:8181'
+      },
+      fee: Number("0.1") * 1e9 // in nanomina (1 billion = 1.0 mina)
+    }
+  ]
+  
+]);
 
-let zkAppKeysBase58: { privateKey: string; publicKey: string } = JSON.parse(
-  await fs.readFile(config.keyPath, 'utf8')
-);
 
-let feepayerKey = PrivateKey.fromBase58(feepayerKeysBase58.privateKey);
-let zkAppKey = PrivateKey.fromBase58(zkAppKeysBase58.privateKey);
+const activeConfig = config.get(process.argv[2]) ;
 
-// set up Mina instance and contract we interact with
-const Network = Mina.Network({
-  // We need to default to the testnet networkId if none is specified for this deploy alias in config.json
-  // This is to ensure the backward compatibility.
-  networkId: (config.networkId ?? DEFAULT_NETWORK_ID) as NetworkId,
-  mina: config.url,
-});
-// const Network = Mina.Network(config.url);
-const fee = Number(config.fee) * 1e9; // in nanomina (1 billion = 1.0 mina)
-Mina.setActiveInstance(Network);
-let feepayerAddress = feepayerKey.toPublicKey();
-let zkAppAddress = zkAppKey.toPublicKey();
-let zkApp = new Add(zkAppAddress);
-
-let sentTx;
-// compile the contract to create prover keys
-console.log('compile the contract...');
-await Add.compile();
-try {
-  // call update() and send transaction
-  console.log('build transaction and create proof...');
-  let tx = await Mina.transaction({ sender: feepayerAddress, fee }, () => {
-    zkApp.update();
-  });
-  await tx.prove();
-  console.log('send transaction...');
-  sentTx = await tx.sign([feepayerKey]).send();
-} catch (err) {
-  console.log(err);
-}
-if (sentTx?.hash() !== undefined) {
-  console.log(`
-Success! Update transaction sent.
-
-Your smart contract state will be updated
-as soon as the transaction is included in a block:
-${getTxnUrl(config.url, sentTx.hash())}
-`);
+if (activeConfig === undefined) {
+  // console.log(commandLineUsage(sections));
+  process.exit(1);
 }
 
-function getTxnUrl(graphQlUrl: string, txnHash: string | undefined) {
-  const txnBroadcastServiceName = new URL(graphQlUrl).hostname
-    .split('.')
-    .filter((item) => item === 'minascan' || item === 'minaexplorer')?.[0];
-  const networkName = new URL(graphQlUrl).hostname
-    .split('.')
-    .filter((item) => item === 'berkeley' || item === 'testworld')?.[0];
-  if (txnBroadcastServiceName && networkName) {
-    return `https://minascan.io/${networkName}/tx/${txnHash}?type=zk-tx`;
+const network = Mina.Network(activeConfig.network);
+Mina.setActiveInstance(network); 
+
+let feePayerBase58 = await Toolkit.initialFeePayer(fs, process.argv[2]);
+
+const feePayerPrivateKey = PrivateKey.fromBase58(feePayerBase58.privateKey);
+const feePayerAccount = feePayerPrivateKey.toPublicKey();
+console.log('Load feePayerPrivateKey ...');
+
+const zkAppClientKeysBase58 = await Toolkit.initialZkAppKey(fs, 'keys/basicRequestClient-zkApp.key');
+console.log('Load basicRequestClient zkAppPrivateKey ...');
+const zkAppClientPrivateKey = PrivateKey.fromBase58(zkAppClientKeysBase58.privateKey);
+const zkAppClientPublicKey = zkAppClientPrivateKey.toPublicKey();
+
+const zkAppOracleKeysBase58 = await Toolkit.initialZkAppKey(fs, 'keys/basicRequestOracle-zkApp.key');
+console.log('Load basicRequestOracle zkAppPrivateKey ...');
+const zkAppOraclePrivateKey = PrivateKey.fromBase58(zkAppOracleKeysBase58.privateKey);
+const zkAppOraclePublicKey = zkAppOraclePrivateKey.toPublicKey();
+
+console.log('Build the contract ... (OracleContract)');
+const zkAppOracle = new OracleContract(zkAppOraclePublicKey);  
+
+await OracleContract.compile();
+
+if(process.argv[3] === "deploy")
+{
+  try {
+    await Toolkit.deploy(activeConfig, feePayerPrivateKey, 
+      zkAppOraclePrivateKey, zkAppOracle, "Deploy zkAppOracle");
+
+  } catch (e) {
+    console.log(e);
   }
-  return `Transaction hash: ${txnHash}`;
-}
+
+} 
